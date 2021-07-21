88abc895d7b76ec56b5cf53eee15c4bed4d3d20a
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-07-21 09:12:26 -0500

    Refactor

--- SeikoColorWheel/SeikoColorWheel.cs
+++ SeikoColorWheel/SeikoColorWheel.cs
@@ -1,16 +1,14 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
 using HarmonyLib;
 using ServerSync;
-using BepInEx.Configuration;
 using UnityEngine;
-using System.IO;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using BepInEx.Bootstrap;
-using System.Linq;
-using System.Collections.Generic;
-using System;
 
 namespace SeikoColorWheel
 {
@@ -20,23 +18,23 @@ namespace SeikoColorWheel
         private const string ModName = "RedSeikoColorWheel";
         private const string ModVersion = "1.0";
         private const string ModGUID = "com.redseiko.comfy";
-        private static readonly ConfigSync configSync = new(ModGUID) { DisplayName = ModName };
-        private static GameObject ColorWheelMenu { get; set; }
-
-        private ConfigEntry<KeyCode> overlayKey;
-
-        public KeyValuePair<string, PluginInfo> ColorfulLights { get; private set; }
+        private static readonly ConfigSync configSync = new(ModGUID) {DisplayName = ModName};
+        private bool clorwards;
+        private bool colorboats;
+        private bool colorlights;
+        private bool colorpieces;
+        private bool colorportals;
 
         private ConfigEntry<Color> fireplacecolor;
-        private ConfigEntry<Color> yachtcolor;
+
+        private ConfigEntry<KeyCode> overlayKey;
         private ConfigEntry<Color> piececolor;
         private ConfigEntry<Color> portalcolor;
         private ConfigEntry<Color> wardcolor;
-        private bool colorlights;
-        private bool clorwards;
-        private bool colorportals;
-        private bool colorpieces;
-        private bool colorboats;
+        private ConfigEntry<Color> yachtcolor;
+        private static GameObject ColorWheelMenu { get; set; }
+
+        public KeyValuePair<string, PluginInfo> ColorfulLights { get; private set; }
 
         private static GameObject menu { get; set; }
         public KeyValuePair<string, PluginInfo> ColorfulWards { get; private set; }
@@ -44,35 +42,24 @@ namespace SeikoColorWheel
         public KeyValuePair<string, PluginInfo> ColorfulPieces { get; private set; }
         public KeyValuePair<string, PluginInfo> YachtClub { get; private set; }
 
-        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
-        {
-            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
-
-            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
-            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
-
-            return configEntry;
-        }
-
-        private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
         public void Awake()
         {
-
             Stream scriptstream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SeikoColorWheel.Libs.SeikoColorWheel.dll");
             byte[] buffer = new byte[scriptstream.Length];
             scriptstream.Read(buffer, 0, buffer.Length);
             Assembly scriptassembly = Assembly.Load(buffer);
 
-            var assetstream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SeikoColorWheel.Assets.colorwheel");
+            var assetstream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("SeikoColorWheel.Assets.colorwheel");
             var bundle = AssetBundle.LoadFromStream(assetstream);
             ColorWheelMenu = bundle.LoadAsset<GameObject>("SeikoColorWheel");
 
             overlayKey =
-               Config.Bind(
-                   "1 - General",
-                   "Key to open overlay",
-                   KeyCode.O,
-                   new ConfigDescription("Key that opens the overlay"));
+                Config.Bind(
+                    "1 - General",
+                    "Key to open overlay",
+                    KeyCode.O,
+                    new ConfigDescription("Key that opens the overlay"));
 
             Assembly assembly = Assembly.GetExecutingAssembly();
             Harmony harmony = new(ModGUID);
@@ -81,14 +68,16 @@ namespace SeikoColorWheel
 
         private void Start()
         {
-            try { 
-            ColorfulLights = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfullights");
-            if (ColorfulLights.Value.Instance != null)
+            try
             {
-                Debug.Log($"loading{ColorfulLights}");
-                ColorfulLights.Value.Instance.Config.TryGetEntry<Color>("Color", "targetFireplaceColor", out fireplacecolor);
+                ColorfulLights = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfullights");
+                if (ColorfulLights.Value.Instance != null)
+                {
+                    Debug.Log($"loading{ColorfulLights}");
+                    ColorfulLights.Value.Instance.Config.TryGetEntry("Color", "targetFireplaceColor",
+                        out fireplacecolor);
                     colorlights = true;
-            }
+                }
             }
             catch
             {
@@ -97,14 +86,14 @@ namespace SeikoColorWheel
             }
 
             try
-            {         
-            ColorfulWards = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulwards");
-            if (ColorfulWards.Value.Instance != null)
             {
-                Debug.Log($"Loading{ColorfulWards}");
-                ColorfulWards.Value.Instance.Config.TryGetEntry<Color>("Color", "targetWardColor", out wardcolor);
+                ColorfulWards = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulwards");
+                if (ColorfulWards.Value.Instance != null)
+                {
+                    Debug.Log($"Loading{ColorfulWards}");
+                    ColorfulWards.Value.Instance.Config.TryGetEntry("Color", "targetWardColor", out wardcolor);
                     clorwards = true;
-            }
+                }
             }
             catch
             {
@@ -113,14 +102,14 @@ namespace SeikoColorWheel
             }
 
             try
-            { 
-            ColorfulPortals = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulportals");
-            if (ColorfulPortals.Value.Instance != null)
             {
-                Debug.Log($"Loading{ColorfulPortals}");
-                ColorfulPortals.Value.Instance.Config.TryGetEntry<Color>("Color", "targetPortalColor", out portalcolor);
+                ColorfulPortals = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulportals");
+                if (ColorfulPortals.Value.Instance != null)
+                {
+                    Debug.Log($"Loading{ColorfulPortals}");
+                    ColorfulPortals.Value.Instance.Config.TryGetEntry("Color", "targetPortalColor", out portalcolor);
                     colorportals = true;
-            }
+                }
             }
             catch
             {
@@ -128,14 +117,15 @@ namespace SeikoColorWheel
                 colorportals = false;
             }
 
-            try { 
-            ColorfulPieces = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulpieces");
-            if (ColorfulPieces.Value.Instance != null)
+            try
             {
-                Debug.Log($"Loading{ColorfulPieces}");
-                ColorfulPieces.Value.Instance.Config.TryGetEntry<Color>("Color", "targetPieceColor", out piececolor);
+                ColorfulPieces = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.colorfulpieces");
+                if (ColorfulPieces.Value.Instance != null)
+                {
+                    Debug.Log($"Loading{ColorfulPieces}");
+                    ColorfulPieces.Value.Instance.Config.TryGetEntry("Color", "targetPieceColor", out piececolor);
                     colorpieces = true;
-            }
+                }
             }
             catch
             {
@@ -145,13 +135,13 @@ namespace SeikoColorWheel
 
             try
             {
-            YachtClub = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.yachtclub");
-            if (YachtClub.Value.Instance != null)
-            {
-                Debug.Log($"Loading{YachtClub}");
-                YachtClub.Value.Instance.Config.TryGetEntry<Color>("Color", "targetFireplaceColor", out yachtcolor);
+                YachtClub = Chainloader.PluginInfos.First(p => p.Key == "redseiko.valheim.yachtclub");
+                if (YachtClub.Value.Instance != null)
+                {
+                    Debug.Log($"Loading{YachtClub}");
+                    YachtClub.Value.Instance.Config.TryGetEntry("Color", "targetFireplaceColor", out yachtcolor);
                     colorboats = true;
-            }
+                }
             }
             catch
             {
@@ -159,6 +149,7 @@ namespace SeikoColorWheel
                 colorboats = false;
             }
         }
+
         private void Update()
         {
             if (Player.m_localPlayer == null)
@@ -166,21 +157,27 @@ namespace SeikoColorWheel
             PatchPlayerInputActive.skipOverlayActiveCheck = true;
             try
             {
-                if (ButtonPressed(overlayKey.Value) && (!Chat.instance || !Chat.instance.HasFocus()) && !Console.IsVisible() && !TextInput.IsVisible() && !StoreGui.IsVisible() && !InventoryGui.IsVisible() && !Menu.IsVisible() && (!TextViewer.instance || !TextViewer.instance.IsVisible()) && !Minimap.IsOpen() && !GameCamera.InFreeFly())
+                if (ButtonPressed(overlayKey.Value) && (!Chat.instance || !Chat.instance.HasFocus()) &&
+                    !Console.IsVisible() && !TextInput.IsVisible() && !StoreGui.IsVisible() &&
+                    !InventoryGui.IsVisible() && !Menu.IsVisible() &&
+                    (!TextViewer.instance || !TextViewer.instance.IsVisible()) && !Minimap.IsOpen() &&
+                    !GameCamera.InFreeFly())
                 {
                     if (menu is null)
                     {
                         menu = Instantiate(ColorWheelMenu);
                         menu.name = "ColorMenu";
                         menu.transform.SetSiblingIndex(menu.transform.GetSiblingIndex() - 4);
-                       
+
                         //add if else statements determining the 
-                        if (colorlights == true)
+                        if (colorlights)
                         {
                             var torchbutton = menu.transform.Find("ColorfulTorches").GetComponent<Button>();
                             torchbutton.onClick.AddListener(() =>
                             {
-                                ColorPicker.Create(fireplacecolor.Value, $"Choose the { ColorfulLights.Value.Instance.name}'s color!", SetColorFireplace, ColorFinished, true);
+                                ColorPicker.Create(fireplacecolor.Value,
+                                    $"Choose the {ColorfulLights.Value.Instance.name}'s color!", SetColorFireplace,
+                                    ColorFinished, true);
                             });
                         }
                         else if (colorlights == false)
@@ -190,12 +187,14 @@ namespace SeikoColorWheel
                         }
 
 
-                        if (clorwards == true)
+                        if (clorwards)
                         {
                             var wardbutton = menu.transform.Find("ColorfulWards").GetComponent<Button>();
                             wardbutton.onClick.AddListener(() =>
                             {
-                                ColorPicker.Create(wardcolor.Value, $"Choose the { ColorfulWards.Value.Instance.name}'s color!", SetColorWard, ColorFinished, true);
+                                ColorPicker.Create(wardcolor.Value,
+                                    $"Choose the {ColorfulWards.Value.Instance.name}'s color!", SetColorWard,
+                                    ColorFinished, true);
                             });
                         }
                         else if (clorwards == false)
@@ -205,12 +204,14 @@ namespace SeikoColorWheel
                         }
 
 
-                        if (colorportals == true)
+                        if (colorportals)
                         {
                             var portalbutton = menu.transform.Find("ColorfulPortals").GetComponent<Button>();
                             portalbutton.onClick.AddListener(() =>
                             {
-                                ColorPicker.Create(portalcolor.Value, $"Choose the { ColorfulPortals.Value.Instance.name}'s color!", SetColorPortal, ColorFinished, true);
+                                ColorPicker.Create(portalcolor.Value,
+                                    $"Choose the {ColorfulPortals.Value.Instance.name}'s color!", SetColorPortal,
+                                    ColorFinished, true);
                             });
                         }
                         else if (colorportals == false)
@@ -219,12 +220,14 @@ namespace SeikoColorWheel
                             portalbutton.gameObject.SetActive(false);
                         }
 
-                        if (colorpieces == true)
+                        if (colorpieces)
                         {
                             var piecebutton = menu.transform.Find("ColorfulPieces").GetComponent<Button>();
                             piecebutton.onClick.AddListener(() =>
                             {
-                                ColorPicker.Create(piececolor.Value, $"Choose the { ColorfulPieces.Value.Instance.name}'s color!", SetColorPiece, ColorFinished, true);
+                                ColorPicker.Create(piececolor.Value,
+                                    $"Choose the {ColorfulPieces.Value.Instance.name}'s color!", SetColorPiece,
+                                    ColorFinished, true);
                             });
                         }
                         else if (colorpieces == false)
@@ -234,12 +237,14 @@ namespace SeikoColorWheel
                         }
 
 
-                        if (colorboats == true)
+                        if (colorboats)
                         {
                             var yachtbutton = menu.transform.Find("YachtClub").GetComponent<Button>();
                             yachtbutton.onClick.AddListener(() =>
                             {
-                                ColorPicker.Create(yachtcolor.Value, $"Choose the { YachtClub.Value.Instance.name}'s color!", SetColorYacht, ColorFinished, true);
+                                ColorPicker.Create(yachtcolor.Value,
+                                    $"Choose the {YachtClub.Value.Instance.name}'s color!", SetColorYacht,
+                                    ColorFinished, true);
                             });
                         }
                         else if (colorboats == false)
@@ -247,7 +252,6 @@ namespace SeikoColorWheel
                             var yachtbutton = menu.transform.Find("YachtClub").GetComponent<Button>();
                             yachtbutton.gameObject.SetActive(false);
                         }
-
                     }
                     else
                     {
@@ -260,6 +264,24 @@ namespace SeikoColorWheel
                 PatchPlayerInputActive.skipOverlayActiveCheck = false;
             }
         }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
         private static bool ButtonPressed(KeyCode button)
         {
             try
@@ -270,37 +292,42 @@ namespace SeikoColorWheel
             {
                 return false;
             }
-
         }
+
         public static void ColorFinished(Color finishedColor)
         {
             Debug.Log("You chose the color " + ColorUtility.ToHtmlStringRGBA(finishedColor));
         }
+
         public void SetColorFireplace(Color currentColor)
         {
             fireplacecolor.Value = currentColor;
         }
+
         public void SetColorWard(Color currentColor)
         {
             wardcolor.Value = currentColor;
         }
+
         public void SetColorYacht(Color currentColor)
         {
             yachtcolor.Value = currentColor;
         }
+
         public void SetColorPiece(Color currentColor)
         {
             piececolor.Value = currentColor;
         }
+
         public void SetColorPortal(Color currentColor)
         {
             portalcolor.Value = currentColor;
         }
 
         [HarmonyPatch(typeof(Menu), "IsVisible")]
-        class PatchPlayerInputActive
+        private class PatchPlayerInputActive
         {
-            public static bool skipOverlayActiveCheck = false;
+            public static bool skipOverlayActiveCheck;
 
             private static bool Prefix(ref bool __result)
             {
@@ -313,16 +340,5 @@ namespace SeikoColorWheel
                 return true;
             }
         }
-        /*
-         * ColorfulTorches
-         * ColorfulPieces
-         * ColorfulWards
-         * ColorfulPortals
-         * YachtClub
-         * 
-         * button names
-         */
-
-
     }
 }
