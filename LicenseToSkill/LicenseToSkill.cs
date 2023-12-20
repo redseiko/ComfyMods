4d14051da5352cc5fe6d82ec0517af944fc83ab4
BruceOfTheBow <bruceofthebow@gmail.com>
2023-12-19 18:50:28 -0800

    LicenseToSkill v1.2.1. Fixed bug where HUD UI for soft death resets after timer. Improved behavior when toggling mod enabled/disabled.

--- LicenseToSkill/LicenseToSkill.cs
+++ LicenseToSkill/LicenseToSkill.cs
@@ -1,5 +1,6 @@
-﻿using System.Reflection;
-
+﻿using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using BepInEx;
 
 using HarmonyLib;
@@ -11,9 +12,11 @@ namespace LicenseToSkill {
   public class LicenseToSkill : BaseUnityPlugin {
     public const string PluginGUID = "redseiko.valheim.comfytools.licensetoskill";
     public const string PluginName = "LicenseToSkill";
-    public const string PluginVersion = "1.2.0";
+    public const string PluginVersion = "1.2.1";
 
     Harmony _harmony;
+
+    public const float DefaultHardDeathCooldown = 600f;
 
     public void Awake() {
       BindConfig(Config);
@@ -23,6 +26,35 @@ namespace LicenseToSkill {
 
     public void OnDestroy() {
       _harmony?.UnpatchSelf();
+    }
+
+    public static void SetHardDeathCoolDown() {
+      if (!Player.m_localPlayer) {
+        return;
+      }
+
+      UpdateHardDeathCooldownTimer();
+
+      if (IsModEnabled.Value) {
+        Player.m_localPlayer.m_hardDeathCooldown = HardDeathCooldownOverride.Value;
+        return;
+      }
+
+      Player.m_localPlayer.m_hardDeathCooldown = DefaultHardDeathCooldown;
+    }
+
+    static void UpdateHardDeathCooldownTimer() {
+      if (!Player.m_localPlayer || Player.m_localPlayer.m_seman == null) {
+        return;
+      }
+
+      StatusEffect hardDeathCooldown = Player.m_localPlayer.m_seman.m_statusEffects.Where(x => x.NameHash() == Player.s_statusEffectSoftDeath).FirstOrDefault();
+
+      if (hardDeathCooldown == null) {
+        return;
+      }
+
+      hardDeathCooldown.m_ttl = HardDeathCooldownOverride.Value * 60f - Player.m_localPlayer.m_timeSinceDeath;
     }
   }
 }
--- LicenseToSkill/Patches/PlayerPatch.cs
+++ LicenseToSkill/Patches/PlayerPatch.cs
@@ -1,10 +1,20 @@
 ﻿using HarmonyLib;
-
+using UnityEngine;
 using static LicenseToSkill.PluginConfig;
 
 namespace LicenseToSkill {
   [HarmonyPatch(typeof(Player))]
   static class PlayerPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Player.OnSpawned))]
+    static void SetupAwakePostfix(ref Player __instance) {
+      if (!IsModEnabled.Value || __instance != Player.m_localPlayer) {
+        return;
+      }
+
+      __instance.m_hardDeathCooldown = HardDeathCooldownOverride.Value;
+    }
+
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Player.HardDeath))]
     static void HardDeathPostfix(ref Player __instance, ref bool __result) {
