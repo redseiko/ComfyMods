98ac07ef5f33e7bb8f3ce11cac00d43ec938494d
redseiko <redseiko@gmail.com>
2024-04-28 11:54:18 -0700

    Update CriticalDice to `v1.7.0`.
    * Updated for the `v0.218.12` PTB patch and `BetterZeeRouter v1.9.0` update.
    * Moved changelog into `CHANGELOG.md`.
    * Bumped up `<LangVersion>` to C# 10.
    * Code clean-up and refactoring.

---
+++ CriticalDice/Core/Handlers/SayHandler.cs
@@ -0,0 +1,127 @@
+﻿namespace CriticalDice;
+
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using BetterZeeRouter;
+
+using UnityEngine;
+
+public sealed class SayHandler : RpcMethodHandler {
+  public static void Register() {
+    RoutedRpcManager.AddHandler("Say", _instance);
+  }
+
+  static readonly SayHandler _instance = new();
+
+  SayHandler() {
+    // ...
+  }
+  
+  public override bool Process(ZRoutedRpc.RoutedRPCData routedRpcData) {
+    routedRpcData.m_parameters.SetPos(0);
+
+    routedRpcData.m_parameters.ReadInt(); // Talker.Type
+    string playerName = routedRpcData.m_parameters.ReadString();
+    routedRpcData.m_parameters.ReadString(); // UserInfo.Gamertag
+    routedRpcData.m_parameters.ReadString(); // UserInfo.NetworkUserId
+    string messageText = routedRpcData.m_parameters.ReadString();
+
+    routedRpcData.m_parameters.SetPos(0);
+
+    if (messageText.StartsWith("!roll", StringComparison.Ordinal)) {
+      ZNet.m_instance.StartCoroutine(ParseRpcSayDataCoroutine(playerName, messageText, routedRpcData.m_targetZDO));
+    }
+
+    return true;
+  }
+
+  static readonly WaitForSeconds _waitInterval = new(seconds: 0.5f);
+  static readonly Regex _htmlTagsRegex = new("<.*?>");
+  static readonly System.Random _random = new();
+
+  static IEnumerator ParseRpcSayDataCoroutine(string playerName, string messageText, ZDOID targetZdo) {
+    yield return _waitInterval;
+
+    long result = 0L;
+    Task<bool> task = Task.Run(() => ParseDiceRoll(messageText, out result));
+
+    while (!task.IsCompleted) {
+      yield return null;
+    }
+
+    if (task.IsFaulted || !task.Result) {
+      yield break;
+    }
+
+    SendDiceRollResponse(
+        _htmlTagsRegex.Replace(playerName, string.Empty),
+        targetZdo,
+        result,
+        "<color=#AEC6D3><b>Server</b></color>",
+        PrivilegeManager.GetNetworkUserId());
+  }
+
+  static void SendDiceRollResponse(
+      string playerName, ZDOID targetZdoId, long result, string senderName, string networkUserId) {
+    ZRoutedRpc.s_instance.InvokeRoutedRPC(
+        ZRoutedRpc.Everybody,
+        targetZdoId,
+        "Say",
+        (int) Talker.Type.Normal,
+        new UserInfo() {
+          Name = senderName,
+          Gamertag = senderName,
+          NetworkUserId = networkUserId
+        },
+        $"{playerName} rolled... {result}",
+        networkUserId);
+  }
+
+  static readonly Regex _diceRollRegex =
+      new(@"^!roll\s+(?:(?<simple>\d+)(?:\s+.*)?$|(?<count>\d*)d(?<faces>\d+)\s*(?<modifier>[\+-]\d+)?(?:\s+.*)?$)");
+
+  static bool ParseDiceRoll(string input, out long result) {
+    result = 0;
+
+    MatchCollection matches = _diceRollRegex.Matches(input);
+
+    if (matches.Count <= 0) {
+      return false;
+    }
+
+    Match match = matches[0];
+
+    if (match.Groups["simple"].Length > 0) {
+      if (!int.TryParse(match.Groups["simple"].Value, out int simple) || simple < 2) {
+        return false;
+      }
+
+      result += _random.Next(simple) + 1;
+      return true;
+    }
+
+    if (!int.TryParse(match.Groups["count"].Value, out int diceCount)) {
+      diceCount = 1;
+    }
+
+    if (!int.TryParse(match.Groups["faces"].Value, out int diceFaces) || diceFaces < 2) {
+      return false;
+    }
+
+    if (int.TryParse(match.Groups["modifier"].Value, out int modifier)) {
+      result += modifier;
+    }
+
+    diceCount = Math.Min(diceCount, 20);
+    diceFaces = Math.Min(diceFaces, 1000);
+
+    for (int i = 0; i < diceCount; i++) {
+      result += _random.Next(diceFaces) + 1;
+    }
+
+    return true;
+  }
+}
--- CriticalDice/CriticalDice.cs
+++ CriticalDice/CriticalDice.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿namespace CriticalDice;
+
 using System.Reflection;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 using BepInEx;
 
@@ -10,143 +8,16 @@ using BetterZeeRouter;
 
 using HarmonyLib;
 
-using UnityEngine;
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+[BepInDependency(BetterZeeRouter.PluginGuid, BepInDependency.DependencyFlags.HardDependency)]
+public sealed class CriticalDice : BaseUnityPlugin {
+  public const string PluginGUID = "redseiko.valheim.criticaldice";
+  public const string PluginName = "CriticalDice";
+  public const string PluginVersion = "1.7.0";
 
-namespace CriticalDice {
-  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-  [BepInDependency(BetterZeeRouter.BetterZeeRouter.PluginGuid, BepInDependency.DependencyFlags.HardDependency)]
-  public class CriticalDice : BaseUnityPlugin {
-    public const string PluginGUID = "redseiko.valheim.criticaldice";
-    public const string PluginName = "CriticalDice";
-    public const string PluginVersion = "1.6.0";
+  void Awake() {
+    Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
 
-    static readonly int _rpcSayHashCode = "Say".GetStableHashCode();
-    static readonly SayHandler _sayHandler = new();
-
-    static readonly Regex _htmlTagsRegex = new("<.*?>");
-    static readonly System.Random _random = new();
-
-    Harmony _harmony;
-
-    public void Awake() {
-      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
-
-      RoutedRpcManager.Instance.AddHandler(_rpcSayHashCode, _sayHandler);
-    }
-
-    public void OnDestroy() {
-      _harmony?.UnpatchSelf();
-    }
-
-    class SayHandler : RpcMethodHandler {
-      const string _rollPrefix = "!roll";
-
-      public override bool Process(ZRoutedRpc.RoutedRPCData routedRpcData) {
-        routedRpcData.m_parameters.SetPos(0);
-
-        // Talker.Type
-        routedRpcData.m_parameters.ReadInt();
-
-        string playerName = routedRpcData.m_parameters.ReadString();
-
-        routedRpcData.m_parameters.ReadString(); // UserInfo.Gamertag
-        routedRpcData.m_parameters.ReadString(); // UserInfo.NetworkUserId
-
-        string messageText = routedRpcData.m_parameters.ReadString();
-
-        routedRpcData.m_parameters.SetPos(0);
-
-        if (messageText.StartsWith(_rollPrefix, StringComparison.Ordinal)) {
-          ZNet.m_instance.StartCoroutine(ParseRpcSayDataCoroutine(playerName, messageText, routedRpcData.m_targetZDO));
-        }
-
-        return true;
-      }
-    }
-
-    static readonly WaitForSeconds _waitInterval = new(seconds: 0.5f);
-
-    static IEnumerator ParseRpcSayDataCoroutine(string playerName, string messageText, ZDOID targetZdo) {
-      yield return _waitInterval;
-
-      long result = 0L;
-      Task<bool> task = Task.Run(() => ParseDiceRoll(messageText, out result));
-
-      while (!task.IsCompleted) {
-        yield return null;
-      }
-
-      if (task.IsFaulted || !task.Result) {
-        yield break;
-      }
-
-      SendDiceRollResponse(
-          _htmlTagsRegex.Replace(playerName, string.Empty),
-          targetZdo,
-          result,
-          "<color=#AEC6D3><b>Server</b></color>",
-          PrivilegeManager.GetNetworkUserId());
-    }
-
-    static void SendDiceRollResponse(
-        string playerName, ZDOID targetZdoId, long result, string senderName, string networkUserId) {
-      ZRoutedRpc.s_instance.InvokeRoutedRPC(
-          ZRoutedRpc.Everybody,
-          targetZdoId,
-          "Say",
-          (int) Talker.Type.Normal,
-          new UserInfo() {
-            Name = senderName,
-            Gamertag = senderName,
-            NetworkUserId = networkUserId
-          },
-          $"{playerName} rolled... {result}",
-          networkUserId);
-    }
-
-    static readonly Regex _diceRollRegex =
-        new(@"^!roll\s+(?:(?<simple>\d+)(?:\s+.*)?$|(?<count>\d*)d(?<faces>\d+)\s*(?<modifier>[\+-]\d+)?(?:\s+.*)?$)");
-
-    static bool ParseDiceRoll(string input, out long result) {
-      result = 0;
-
-      MatchCollection matches = _diceRollRegex.Matches(input);
-
-      if (matches.Count <= 0) {
-        return false;
-      }
-
-      Match match = matches[0];
-
-      if (match.Groups["simple"].Length > 0) {
-        if (!int.TryParse(match.Groups["simple"].Value, out int simple) || simple < 2) {
-          return false;
-        }
-
-        result += _random.Next(simple) + 1;
-        return true;
-      }
-
-      if (!int.TryParse(match.Groups["count"].Value, out int diceCount)) {
-        diceCount = 1;
-      }
-
-      if (!int.TryParse(match.Groups["faces"].Value, out int diceFaces) || diceFaces < 2) {
-        return false;
-      }
-
-      if (int.TryParse(match.Groups["modifier"].Value, out int modifier)) {
-        result += modifier;
-      }
-
-      diceCount = Math.Min(diceCount, 20);
-      diceFaces = Math.Min(diceFaces, 1000);
-
-      for (int i = 0; i < diceCount; i++) {
-        result += _random.Next(diceFaces) + 1;
-      }
-
-      return true;
-    }
+    SayHandler.Register(); 
   }
 }
