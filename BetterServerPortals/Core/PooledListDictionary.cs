5d5c15aa355a4cd21076725831a31bf598c607ff
redseiko <redseiko@users.noreply.github.com>
2024-10-05 22:48:37 -0700

    [WIP] Check-in WIP changes for BetterServerPortals update to `v1.7.0`.

---
+++ BetterServerPortals/Core/PooledListDictionary.cs
@@ -0,0 +1,58 @@
+﻿namespace BetterServerPortals;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.Pool;
+
+public sealed class PooledListDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, List<TValue>>> {
+  readonly Dictionary<TKey, List<TValue>> _keyToListDictionary = [];
+
+  public List<TValue> this[TKey index] {
+    get {
+      if (!_keyToListDictionary.TryGetValue(index, out List<TValue> list)) {
+        list = _listPool.Get();
+        _keyToListDictionary[index] = list;
+      }
+
+      return list;
+    }
+  }
+
+  public void Add(TKey key, TValue value) {
+    this[key].Add(value);
+  }
+
+  public void Clear() {
+    foreach (List<TValue> list in _keyToListDictionary.Values) {
+      _listPool.Release(list);
+    }
+
+    _keyToListDictionary.Clear();
+  }
+
+  public IEnumerator<KeyValuePair<TKey, List<TValue>>> GetEnumerator() {
+    return _keyToListDictionary.GetEnumerator();
+  }
+
+  IEnumerator IEnumerable.GetEnumerator() {
+    return GetEnumerator();
+  }
+
+  static readonly ObjectPool<List<TValue>> _listPool =
+      new(
+          createFunc: CreateList,
+          actionOnGet: ClearList,
+          actionOnRelease: ClearList,
+          actionOnDestroy: ClearList,
+          collectionCheck: false,
+          defaultCapacity: 0);
+
+  static List<TValue> CreateList() {
+    return [];
+  }
+
+  static void ClearList(List<TValue> list) {
+    list.Clear();
+  }
+}
