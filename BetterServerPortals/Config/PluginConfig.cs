6188035adbf5da42fcd71fb0d91e7145165fe71b
redseiko <redseiko@gmail.com>
2024-10-12 13:37:27 -0700

    [WIP] Check-in more WIP changes for BetterServerPortals update to `v1.7.0`.

--- BetterServerPortals/Config/PluginConfig.cs
+++ BetterServerPortals/Config/PluginConfig.cs
@@ -5,6 +5,8 @@ using BepInEx.Configuration;
 public static class PluginConfig {
   public static ConfigEntry<float> ConnectPortalCoroutineWait { get; private set; }
 
+  public static ConfigEntry<string> RandomPortalsAccessListFilename { get; private set; }
+
   public static void BindConfig(ConfigFile config) {
     ConnectPortalCoroutineWait =
         config.Bind(
@@ -12,5 +14,12 @@ public static class PluginConfig {
             "connectPortalCoroutineWait",
             5f,
             "Wait time (seconds) when ConnectPortal coroutine yields.");
+
+    RandomPortalsAccessListFilename =
+        config.Bind(
+            "RandomPortals",
+            "accessListFilename",
+            "bsp-random-portals-access-list.txt",
+            "Filename for SyncedList containing SteamIds allowed to set RandomPortal tags.");
   }
 }
