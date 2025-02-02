d5f8adb483a62b4dcf702b1fd5525001691b0a35
redseiko <redseiko@users.noreply.github.com>
2025-02-01 18:48:47 -0800

    Check-in a variant of `SQlite.cs` and the precompiled Window x64 version of `System.Data.SQLite.dll`.

---
+++ Entitlement/Database/Tables/PlayerTitle.cs
@@ -0,0 +1,11 @@
+﻿namespace Entitlement.Database;
+
+using SQLite;
+
+public sealed class PlayerTitle {
+  [PrimaryKey]
+  [AutoIncrement]
+  public int TitleId { get; set; }
+
+  public string TitleName { get; set; }
+}
