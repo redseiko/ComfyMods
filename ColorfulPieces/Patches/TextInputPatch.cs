39a3691c91c02681622e4af3dd40410eaa0e0ce8
hhliow <hxliow@gmail.com>
2025-03-17 20:58:10 -0400

    Prevent TextInput.IsVisible() from attempting to create color picker while in main menu

--- ColorfulPieces/Patches/TextInputPatch.cs
+++ ColorfulPieces/Patches/TextInputPatch.cs
@@ -9,6 +9,10 @@ static class TextInputPatch {
   [HarmonyPostfix]
   [HarmonyPatch(nameof(TextInput.IsVisible))]
   static void IsVisiblePostfix(ref bool __result) {
+    if (FejdStartup.instance != null && FejdStartup.instance.isActiveAndEnabled) {
+        // Do nothing while in the main menu.
+        return;
+    }  
     if (!__result && IsModEnabled.Value && ColorPickerController.Instance.IsVisible()) {
       __result = true;
     }
