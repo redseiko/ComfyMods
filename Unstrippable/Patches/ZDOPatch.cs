796e848b1606c3fdcae980015c55627d27f8854f
redseiko <redseiko@users.noreply.github.com>
2026-05-22 16:07:59 -0700

    Update Unstrippable to `v1.1.0`.
    * Fixed for the `v0.221.13` PTB patch.
    * Updated mod icon.

--- Unstrippable/Patches/ZDOPatch.cs
+++ Unstrippable/Patches/ZDOPatch.cs
@@ -9,16 +9,15 @@ using static PluginConfig;
 [HarmonyPatch(typeof(ZDO))]
 static class ZDOPatch {
   [HarmonyPrefix]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int) })]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int), typeof(long) })]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int), typeof(int) })]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int), typeof(Quaternion) })]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int), typeof(string) })]
-  [HarmonyPatch(nameof(ZDO.Strip), new[] { typeof(int), typeof(byte[]) })]
-  [HarmonyPatch(nameof(ZDO.StripConvert), new[] { typeof(ZDOID), typeof(int), typeof(long) })]
-  [HarmonyPatch(nameof(ZDO.StripConvert), new[] { typeof(ZDOID), typeof(int), typeof(Vector3) })]
-  [HarmonyPatch(nameof(ZDO.StripConvert), new[] { typeof(ZDOID), typeof(int), typeof(float) })]
-  [HarmonyPatch(nameof(ZDO.StripLong), new[] { typeof(int) })]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int)])]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int), typeof(long)])]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int), typeof(int)])]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int), typeof(Quaternion)])]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int), typeof(string)])]
+  [HarmonyPatch(nameof(ZDO.Strip), [typeof(int), typeof(byte[])])]
+  [HarmonyPatch(nameof(ZDO.StripConvert), [typeof(ZDOID), typeof(int), typeof(long)])]
+  [HarmonyPatch(nameof(ZDO.StripConvert), [typeof(ZDOID), typeof(int), typeof(Vector3)])]
+  [HarmonyPatch(nameof(ZDO.StripLong), [typeof(int)])]
   static bool StripPrefix(ref bool __result) {
     if (IsModEnabled.Value) {
       __result = false;
